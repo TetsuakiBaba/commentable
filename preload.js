15b3e967ce1efe35855b85d20d16bab7b1821d37
chihiros <szrkw2ui2a@gmail.com>
2022-12-05 20:49:32 +0900

    del: 不要な改行削除

--- preload.js
+++ preload.js
@@ -7,8 +7,4 @@ window.addEventListener('DOMContentLoaded', () => {
     for (const type of ['chrome', 'node', 'electron']) {
         replaceText(`${type}-version`, process.versions[type])
     }
-
-
-
 })
-
