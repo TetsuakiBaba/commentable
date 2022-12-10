8a2c90a698c7188c792202840e23d91dc2e118db
chihiros <szrkw2ui2a@gmail.com>
2022-12-11 03:29:58 +0900

    del: gotDevicesは使われていないので削除

--- public/js/utils.js
+++ public/js/utils.js
@@ -39,16 +39,4 @@ function getDevices() {
   return navigator.mediaDevices.enumerateDevices();
 }
 
-function gotDevices(deviceInfos) {
-  window.deviceInfos = deviceInfos;
-  for (const deviceInfo of deviceInfos) {
-    const option = document.createElement("option");
-    option.value = deviceInfo.deviceId;
-    if (deviceInfo.kind === "videoinput") {
-      option.text = deviceInfo.label || `Camera ${videoSelect.length + 1}`;
-      videoSelect.appendChild(option);
-    }
-  }
-}
-
 const videoSelect = document.querySelector("select#videoSource");
