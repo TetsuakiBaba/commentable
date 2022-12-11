b0898b413dce84fc7939a9d6a961a757fdb1d0cc
chihiros <szrkw2ui2a@gmail.com>
2022-12-12 00:26:27 +0900

    add: animetion.jsの追加

---
+++ js/animetion.js
@@ -0,0 +1,37 @@
+// アニメーション用のクラス
+class Position {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  setPosition(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  getPosition() {
+    return [this.x, this.y];
+  }
+}
+
+class Animetion {
+  constructor(fps) {
+    this.position = Position(); // 現在の位置
+    this.size = height / 20; // 現在のサイズ
+    this.time = 5000;  // 移動にかかるトータル時間(ms)
+    this.frame = fps * this.time / 1000; // 移動にかかるフレーム数
+  }
+
+  setSize(size) {
+    this.size = size;
+  }
+
+  updatePosition(x, y) {
+    this.position.setPosition(x, y);
+  }
+
+  print() {
+    console.log(this.position.getPosition());
+  }
+}
