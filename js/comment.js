9d6dce45fd89cce380d78859531487a0660da1ee
chihiros <szrkw2ui2a@gmail.com>
2022-12-16 01:31:54 +0900

    fix: 絵文字とテキストで移動方法を変更する

--- js/comment.js
+++ js/comment.js
@@ -68,11 +68,17 @@ class Comment {
 
   update(_FRAME) {
     if (this.life > 0) {
-      // this.alpha = this.life;
-      this.size = abs((height / 20) * sin(0.5 * PI * this.life / _FRAME));
-      this.life--; // 0 - 255
-      if (this.life == 0) {
-        this.flg_img = false;
+      if (this.flg_emoji) {
+        // 絵文字の場合の移動
+        // this.alpha = this.life;
+        this.size = abs((height / 20) * sin(0.5 * PI * this.life / _FRAME));
+        this.life--; // 0 - 255
+        if (this.life == 0) {
+          this.flg_img = false;
+        }
+      } else {
+        // テキストの場合の移動
+
       }
     }
     return;
