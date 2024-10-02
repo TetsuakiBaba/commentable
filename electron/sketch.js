0482b23d3c97c1e557d9ed513e13fed1f1d99879
tetsuakibaba <tetsuaki.baba@gmail.com>
2024-10-02 18:21:34 +0900

    commentable.funのドメイン廃止による変更

--- electron/sketch.js
+++ electron/sketch.js
@@ -77,7 +77,7 @@ function toggleQR(checked, position, room) {
     const qrCode = new QRCodeStyling({
         "width": qr_width,
         "height": qr_height,
-        "data": `https://commentable.fun/?room=${encodeURI(room)}&v=${version}`,
+        "data": `https://bbcommentable.herokuapp.com/?room=${encodeURI(room)}&v=${version}`,
         "margin": qr_width / 15,
         "qrOptions": { "typeNumber": "0", "mode": "Byte", "errorCorrectionLevel": "Q" },
         "imageOptions": { "hideBackgroundDots": true, "imageSize": 0.4, "margin": 0 },
